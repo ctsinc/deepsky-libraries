3207475c2f29b5fe2676a72adcb7b245ea555bcd
hassaku <hassaku.sc@gmail.com>
2025-02-16 16:50:14 +0900

    add protocol-v1 project

---
+++ libraries/deepsky-protocol-v1/test/deepsky_protocol_v1_test.dart
@@ -0,0 +1,16 @@
+import 'package:deepsky_protocol_v1/deepsky_protocol_v1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
